3cba9d88e304c27b3c7a2f70b5dea7a7d111cd1c
1285963544@qq.com <1285963544@qq.com>
2018-01-03 09:44:04 +0800

    提交至开发分支

---
+++ src/main/java/com/shsxt/service/UserService.java
@@ -0,0 +1,14 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/1/3.
+ */
+public interface UserService {
+
+    /**
+     * 用户登录
+     * @param userName
+     * @param passworld
+     */
+    public void login (String userName, String passworld) ;
+}
